19e9bc2be8daf8ab9ea368cbbc021a4bd7827ca6
Rob Morgan <robleemorgan@gmail.com>
2026-03-08 10:38:49 +0800

    w-auto divider to fix iphone layout issue

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -7,7 +7,7 @@ export default function Divider({ header }) {
     ${
       header
         ? "h-40 opacity-20 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-        : "h-8 opacity-80 block mx-auto"
+        : "h-8 opacity-80 block mx-auto w-auto"
     }
     mix-blend-multiply saturate-120 transition-all duration-500
   `}
